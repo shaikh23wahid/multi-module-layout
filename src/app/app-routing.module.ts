b4241b6486b60510c6f067cf67d9878ee45c98fb
Wahid Shaikh <shaikh23wahid@gmail.com>
2021-06-05 21:26:39 +0530

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BackendLayoutComponent } from './layouts/backend-layout/backend-layout.component';
+import { DefaultLayoutComponent } from './layouts/default-layout/default-layout.component';
+import { BACKEND_ROUTES } from './routes/backend-layout-routes';
+import { DEFAULT_ROUTES } from './routes/default-layout-routes';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DefaultLayoutComponent,
+    children: DEFAULT_ROUTES
+  },
+  {
+    path: 'backend',
+    component: BackendLayoutComponent,
+    children: BACKEND_ROUTES
+  },
+  {
+    path: '**',
+    redirectTo: 'cms/404'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DefaultLayoutComponent } from './layouts/default-layout/default-layout.component';
+import { BackendLayoutComponent } from './layouts/backend-layout/backend-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DefaultLayoutComponent,
+    BackendLayoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
